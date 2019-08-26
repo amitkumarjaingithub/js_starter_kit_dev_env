69fa4cdb528f53eaa356ff0605fbdafe8bd38fd5
amit jain <amitjain@amits-air.home>
2019-08-26 22:39:06 +0100

    deployment setup

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,5 @@
 export default function getBaseUrl() {
-  return 'http://localhost:3001/';
-
-//http://localhost:3001/  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://mysterious-dawn-16770.herokuapp.com/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://mysterious-dawn-16770.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,23 +1,30 @@
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'inline-source-map',
   noInfo: false,
-  entry:[
+  entry: [
     path.resolve(__dirname, 'src/index')
   ],
   target: 'web',
-  output:{
-    path:path.resolve(__dirname, 'src'),
-    publicPath:'/',
-    filename:'bundle.js'
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
   },
-  plugins:[],
-  module:{
-    loaders:[
-      {test:/\.js$/,exclude:/node_modules/,loaders:['babel'] },
-      {test:/\.css$/, loaders:['style','css']}
+  plugins: [
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
     ]
   }
 }
